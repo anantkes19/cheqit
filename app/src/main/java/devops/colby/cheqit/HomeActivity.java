f6262a24fdae4b2d11ec33426d8351f264e47401
Austin Nantkes <anantkes@colby.edu>
2018-01-16 15:07:29 -0500

    Changed home menu layout

--- app/src/main/java/devops/colby/cheqit/HomeActivity.java
+++ app/src/main/java/devops/colby/cheqit/HomeActivity.java
@@ -3,47 +3,29 @@ package devops.colby.cheqit;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
     final Context context = this;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_history:
-                    Intent detailIntent = new Intent(context,HistoryActivity.class);
-                    startActivity(detailIntent);
-                    return true;
-                case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final TextView mTextMessage = findViewById(R.id.title);
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(context,HistoryActivity.class);
+                startActivity(detailIntent);
+            }
+        });
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
